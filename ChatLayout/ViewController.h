1ac3fc6ee4f12024747e8db435bcd584e252ef0e
zhb1991nm <826071448@qq.com>
2014-12-28 13:21:58 +0800

    Initial Commit

---
+++ ChatLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatLayout
+//
+//  Created by 张浩波 on 14/12/28.
+//  Copyright (c) 2014年 zhb1991nm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
